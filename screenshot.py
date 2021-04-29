a5a5b39c2203baf2aa53d7ba0d82d806dc663925
yukkuri-mint <nsbn98hmphx@gmail.com>
2021-04-29 21:45:59 +0900

    Python

--- screenshot.py
+++ screenshot.py
@@ -1,13 +1,16 @@
 import tkinter as tk
 
-class Application(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title('Screen Shot')
-        self.geometry('500x300')
-        self.config(bg='white')
-        self.attributes('-transparentcolor', 'white')
+def motion(event):
+    x, y = event.x, event.y
+    print('{}, {}'.format(x, y))
+
 
 if __name__ == '__main__':
-    app = Application()
-    app.mainloop()
+    root = tk.Tk()
+
+    root.geometry('100x100')
+    root.config(bg='white')
+    root.attributes('-transparentcolor', 'white')
+
+    root.bind('<Motion>', motion)
+    root.mainloop()
